482a02ff85d73b11f0672fb366976a73d9699b1a
Daniel <danieldoamaralhillesheim@gmail.com>
2025-05-14 23:08:31 -0300

    Correção dos e-mails

--- Aulas/M1S9/meu-app-react/src/App.tsx
+++ Aulas/M1S9/meu-app-react/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Cronometro from './components/Cronometro';
+import Email from './components/Emails';
 
 function App(){
   return(
     <>
       <Cronometro />
+      <Email />
     </>
   )
 }
