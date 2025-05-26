195bf61534ad454cf5bda78dec4a162e994ad2c1
Daniel <danieldoamaralhillesheim@gmail.com>
2025-05-26 19:17:40 -0300

    EX1 ao EX6 M1S9

--- Atividades/FuturoDEV S4/JS/Menu.js
+++ Atividades/FuturoDEV S4/JS/Menu.js
@@ -1,105 +1,3 @@
-// let num1 = 0;
-
-// const soma = (pNum1, pNum2) => pNum1 + pNum2;
-// const subtrcao = (pNum1, pNum2) => pNum1 - pNum2;
-// const pNumPar = (numero) => numero;
-// const pNumImpar = (numero) => numero;
-// const contarPrimos = (limite) => limite;
-
-// function Menu() {
-//     let continuar = true;
-
-//     while (continuar) {
-//         console.log("Escolha um das opções\n[1]Soma\n[2]Subtração\n[3]Quantidade números pares\n[4]Quantidade números impares\n[5]Quantidade números primos\n[6]Sair");
-
-//         const escolha = prompt("Digite o número do opção desejada: ")
-
-//         if (escolha === "6") {
-//             console.log("Saindo da cauculadora...")
-//             continuar = false;
-//             break;
-//         }
-
-//         let num1 = parseInt(prompt("Digite o primeiro numero: "));
-//         let num2 = parseInt(prompt("Digite o segundo numero: "));
-//         let resultado;
-//         let numero = prompt("Digite o número para verificar se seus antecessores são pares: ");
-//         let contador = 1;
-//         let ContarPrimos = 10;
-//         ContP = contarPrimos(ContarPrimos);
-
-//         switch (escolha) {
-//             case "1":
-//                 resultado = soma(num1, num2);
-//                 console.log("O resultado da soma:" + resultado);
-//                 break;
-//             case "2":
-//                 resultado = subtrcao(num1, num2);
-//                 console.log("O resultado da subtração:" + resultado);
-//                 break;
-//             case "3":
-//                 numero = numero;
-//                 contador = contador;
-//                 resultado = pNumPar;
-//                 console.log(resultado);
-//             case "4":
-//                 numero = numero;
-//                 contador = contador;
-//                 resultado = pNumImpar;
-//                 console.log(resultado);
-//             case "5":
-//                 limite = limite;
-//                 resultado = contarPrimos;
-//                 console.log(resultado);
-//             default:
-//                 console.log("Opção inválida!!!");
-//                 break;
-//         }
-//     }
-// }
-
-// function pNumPar() {
-//     while (contador <= numero) {
-//         if (contador % 2 === 0) {
-//             console.log("o número " + contador + " é Par.");
-//         }
-//         else if (contador > numero) {
-//             console.log("o número " + contador + " é Impar.");
-//         }
-
-//         contador++;
-//     }
-// }
-
-// function pNumImpar() {
-//     while (contador <= numero) {
-//         if (contador % 2 > 0) {
-//             console.log("o número " + contador + " é Impar.");
-//         }
-//         else if (contador > numero) {
-//             console.log("o número " + contador + " é Par.");
-//         }
-
-//         contador++;
-//     }
-// }
-
-// function ehPrimo(num) {
-//     if (num < 2) return false;
-//     for (let i = 2; i <= Math.sqrt(num); i++) {
-//         if (num % i === 0) return false;
-//     }
-//     return true;
-// }
-
-// function contarPrimos(limite) {
-//     let contador = 0;
-//     for (let i = 2; i <= limite; i++) {
-//         if (ehPrimo(i)) contador++;
-//     }
-//     return contador;
-// }
-
 const soma = (pNum1, pNum2) => pNum1 + pNum2;
 const subtracao = (pNum1, pNum2) => pNum1 - pNum2;
 
@@ -212,3 +110,105 @@ function Menu() {
 }
 
 Menu();
+
+// let num1 = 0;
+
+// const soma = (pNum1, pNum2) => pNum1 + pNum2;
+// const subtrcao = (pNum1, pNum2) => pNum1 - pNum2;
+// const pNumPar = (numero) => numero;
+// const pNumImpar = (numero) => numero;
+// const contarPrimos = (limite) => limite;
+
+// function Menu() {
+//     let continuar = true;
+
+//     while (continuar) {
+//         console.log("Escolha um das opções\n[1]Soma\n[2]Subtração\n[3]Quantidade números pares\n[4]Quantidade números impares\n[5]Quantidade números primos\n[6]Sair");
+
+//         const escolha = prompt("Digite o número do opção desejada: ")
+
+//         if (escolha === "6") {
+//             console.log("Saindo da cauculadora...")
+//             continuar = false;
+//             break;
+//         }
+
+//         let num1 = parseInt(prompt("Digite o primeiro numero: "));
+//         let num2 = parseInt(prompt("Digite o segundo numero: "));
+//         let resultado;
+//         let numero = prompt("Digite o número para verificar se seus antecessores são pares: ");
+//         let contador = 1;
+//         let ContarPrimos = 10;
+//         ContP = contarPrimos(ContarPrimos);
+
+//         switch (escolha) {
+//             case "1":
+//                 resultado = soma(num1, num2);
+//                 console.log("O resultado da soma:" + resultado);
+//                 break;
+//             case "2":
+//                 resultado = subtrcao(num1, num2);
+//                 console.log("O resultado da subtração:" + resultado);
+//                 break;
+//             case "3":
+//                 numero = numero;
+//                 contador = contador;
+//                 resultado = pNumPar;
+//                 console.log(resultado);
+//             case "4":
+//                 numero = numero;
+//                 contador = contador;
+//                 resultado = pNumImpar;
+//                 console.log(resultado);
+//             case "5":
+//                 limite = limite;
+//                 resultado = contarPrimos;
+//                 console.log(resultado);
+//             default:
+//                 console.log("Opção inválida!!!");
+//                 break;
+//         }
+//     }
+// }
+
+// function pNumPar() {
+//     while (contador <= numero) {
+//         if (contador % 2 === 0) {
+//             console.log("o número " + contador + " é Par.");
+//         }
+//         else if (contador > numero) {
+//             console.log("o número " + contador + " é Impar.");
+//         }
+
+//         contador++;
+//     }
+// }
+
+// function pNumImpar() {
+//     while (contador <= numero) {
+//         if (contador % 2 > 0) {
+//             console.log("o número " + contador + " é Impar.");
+//         }
+//         else if (contador > numero) {
+//             console.log("o número " + contador + " é Par.");
+//         }
+
+//         contador++;
+//     }
+// }
+
+// function ehPrimo(num) {
+//     if (num < 2) return false;
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0) return false;
+//     }
+//     return true;
+// }
+
+// function contarPrimos(limite) {
+//     let contador = 0;
+//     for (let i = 2; i <= limite; i++) {
+//         if (ehPrimo(i)) contador++;
+//     }
+//     return contador;
+// }
